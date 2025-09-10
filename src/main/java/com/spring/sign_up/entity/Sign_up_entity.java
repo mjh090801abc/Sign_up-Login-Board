b9b9be4479d6000b7854b48b429fdfc7c96c5399
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-10 20:19:58 +0900

    주석 추가

--- src/main/java/com/spring/sign_up/entity/Sign_up_entity.java
+++ src/main/java/com/spring/sign_up/entity/Sign_up_entity.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity(name = "users") // DB 테이블 이름 정의
+@Entity(name = "users") // DB 테이블 이름 정의.
 public class Sign_up_entity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
