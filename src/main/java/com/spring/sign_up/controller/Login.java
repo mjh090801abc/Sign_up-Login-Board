965645a8a28a78315044808df457f60cdaab1a71
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-10 20:02:20 +0900

    공백 추가

--- src/main/java/com/spring/sign_up/controller/Login.java
+++ src/main/java/com/spring/sign_up/controller/Login.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class Login {
 
+
     @GetMapping("/login")
     public String login_Form() {
         return "login_form";
