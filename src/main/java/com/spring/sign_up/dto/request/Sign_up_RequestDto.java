4bc495d3475432742679a45480709356c93c6d6e
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-10 20:30:00 +0900

    새로운 Dto 생성 + 어노테이션 추가

---
+++ src/main/java/com/spring/sign_up/dto/request/Sign_up_RequestDto.java
@@ -0,0 +1,4 @@
+package com.spring.sign_up.dto.request;
+
+public class Sign_up_RequestDto {
+}
--- src/main/java/com/spring/sign_up/dto/request/Sign_up_requestDto.java
+++ src/main/java/com/spring/sign_up/dto/request/Sign_up_requestDto.java
@@ -1,15 +1,9 @@
-//package com.spring.sign_up.dto.request;
-//
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@NoArgsConstructor
-//@Getter
-//public class Sign_up_requestDto {
-//
-//    private String username;
-//
-//    private String password;
-//
-//    private String email;
-//}
+package com.spring.sign_up.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class Sign_up_RequestDto {
+}
