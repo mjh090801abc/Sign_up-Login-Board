1db4ff38610a1891705f65ac8a6397f5a96eb8d3
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-10 20:15:31 +0900

    password 다시 정의

--- src/main/java/com/spring/sign_up/entity/Sign_up_entity.java
+++ src/main/java/com/spring/sign_up/entity/Sign_up_entity.java
@@ -25,6 +25,6 @@ public class Sign_up_entity {
 
     public Sign_up_entity(String username, String password, String email) {
         this.username = username;
-
+        this.password = password;
     }
 }
