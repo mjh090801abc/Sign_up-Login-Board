7014cb0a5efb172e07b4ef871ce403a2ca719b38
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-11 19:39:12 +0900

    마이페이지 안에 들어갈 username, email, join_date(가입일) 등을 일단 테스트용으로 추가(DB 넣기 전) + myPage.html로 넘어가기

--- src/main/java/com/spring/sign_up/controller/Dashboard.java
+++ src/main/java/com/spring/sign_up/controller/Dashboard.java
@@ -1,6 +1,7 @@
 package com.spring.sign_up.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,15 @@ public class Dashboard {
     @GetMapping("/dashboard")
     public String dashboard_Form() {
         return "mainPage/dashboard";
+    }
+
+    @GetMapping("/myPage")
+    public String myPage_Form(Model model) {
+
+        model.addAttribute("username", "test_username");
+        model.addAttribute("email", "test_email");
+        model.addAttribute("join_date", "test_join_date");
+
+        return "mainPage/myPage";
     }
 }
