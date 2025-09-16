6893dfd179256e63583288f3a130d2835fea91b1
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-16 17:50:32 +0900

    데이티를 넣을 리스트 생성

--- src/main/java/com/spring/sign_up/controller/Sign_up.java
+++ src/main/java/com/spring/sign_up/controller/Sign_up.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Sign_up {
 
     @Autowired
-    Sign_up_service service;
+    Sign_up_service service; // service는 별명
 
     // 회원가입 폼 열기
     @GetMapping("/sign_up")
@@ -32,7 +32,8 @@ public class Sign_up {
                                 Model model) {
 
         List<Sign_up_entity> sign_up_entities = new ArrayList<>();
-        sign_up_entities =
+        sign_up_entities = service.getLoginData();
+
 
         // 입력값을 모델에 담기
         model.addAttribute("username", username); // @RequestParam 으로 사용자에게 받은 데이터를 보여주는 역할
