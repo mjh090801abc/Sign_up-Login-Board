429d28b1d3bef94f2d90d791bd3ec90b38bfadeb
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-17 18:08:41 +0900

    DB에 연결하기 좋게 코드 변경

--- src/main/java/com/spring/sign_up/controller/Sign_up.java
+++ src/main/java/com/spring/sign_up/controller/Sign_up.java
@@ -1,7 +1,5 @@
 package com.spring.sign_up.controller;
 
-import com.spring.sign_up.entity.Sign_up_entity;
-import com.spring.sign_up.service.Sign_up_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,15 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class Sign_up {
-
-    @Autowired
-    Sign_up_service service; // service는 별명
-
+    
     // 회원가입 폼 열기
     @GetMapping("/sign_up")
     public String SignUp_form() {
@@ -30,10 +22,6 @@ public class Sign_up {
                                 @RequestParam String password,
                                 @RequestParam String email,
                                 Model model) {
-
-        List<Sign_up_entity> sign_up_entities = new ArrayList<>();
-        sign_up_entities = service.getLoginData();
-
 
         // 입력값을 모델에 담기
         model.addAttribute("username", username); // @RequestParam 으로 사용자에게 받은 데이터를 보여주는 역할
