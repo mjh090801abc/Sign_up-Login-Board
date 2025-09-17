3100b0d7dec4659bfbff91ec77ccb22d7964d9e3
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-17 19:57:22 +0900

    코드 에러 수정

--- src/main/java/com/spring/sign_up/repository/UserRepository.java
+++ src/main/java/com/spring/sign_up/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.spring.sign_up.repository;
 import com.spring.sign_up.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // JpaRepository<User, Long> : User entity를 DB랑 연결 시켜줌
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // 이름만 쓰면 sql 자동 생성
-    User findByUsername(String username);
+    // SELECT * FROM user WHERE username = "내가 넣은 이름": 이런식으로 자동으로 생성
+    Optional<User> findByUsername(String username);
 }
